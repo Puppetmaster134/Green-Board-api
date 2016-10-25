4c4863ed236da60d87bd4c19ed23990a0b3e0ec6
Brian <bzimmerman@csumb.edu>
2016-10-25 08:08:34 -0400

    Added endpoint for writing new trails

--- api.php
+++ api.php
@@ -41,12 +41,62 @@ $app->get('/GetTrailById/{id}', function (Request $request, Response $response)
        $stmt = $this->db->prepare($sql);
        $stmt->execute(array(':id'=>$id));
        $result = $stmt->fetch();
-       $response->getBody()->write($result['trailInfo']);
+       $response->getBody()->write($result['trailObj']);
        return $response;
     }
     $response->getBody()->write("Invalid API key");
     return $response;
 
+});
+
+$app->get('/WriteTrailToDB/', function (Request $request, Response $response)
+{
+    $params = $request->getQueryParams();
+    $sql = "SELECT COUNT(*) count FROM user WHERE api_key=:key LIMIT 1";
+    $stmt = $this->db->prepare($sql);
+    $stmt->execute(array(':key'=>$params['key']));
+    $result = $stmt->fetch();
+
+    if($result['count'] > 0 && isset($params['lat']) && isset($params['lng']) && isset($params['trailObj']))
+    {
+       $sql = "INSERT INTO trail (trailInfo,lat,lng,trailObj) VALUES('Empty description',:lat,:lng,:trailObj)";
+       $stmt = $this->db->prepare($sql);
+       $stmt->execute(array(':lat'=>$params['lat'],':lng'=>$params['lng'],':trailObj'=>$params['trailObj']));
+       $response->getBody()->write("Trail added successfully" . " " . $params['lat'] . " " . $params['lng'] . " " . $params['trailObj']);
+       return $response;
+    }
+    $response->getBody()->write("Invalid API key");
+    return $response;
+});
+
+$app->get('/GetTrailInArea/', function (Request $request, Response $response)
+{
+    $params = $request->getQueryParams();
+    $sql = "SELECT COUNT(*) count FROM user WHERE api_key=:key LIMIT 1";
+    $stmt = $this->db->prepare($sql);
+    $stmt->execute(array(':key'=>$params['key']));
+    $result = $stmt->fetch();
+
+    if($result['count'] > 0 && isset($params['minLat']) && isset($params['minLng']) && isset($params['maxLat']) && isset($params['maxLng']))
+    {
+	
+	   $maxResults = 10;
+	   if(isset($params['maxResults']))
+	   {
+		   $maxResults = $params['maxResults'];
+	   }
+	   
+	   
+	   /*
+       $sql = "SELECT * FROM trail WHERE (lat > :minLat) AND (lat < :maxLat) AND (lng > :minLng) AND (lng < :maxLng)  LIMIT :maxResults";
+       $stmt = $this->db->prepare($sql);
+       $stmt->execute(array(':minLat'=>$params['minLat'],':maxLat'=>$params['maxLat'],':minLng'=>$params['minLng'],':maxLng'=>$params['maxLng'],':maxResults'=>$maxResults));
+       */
+	   $response->getBody()->write("Endpoint is not developed yet");
+       return $response;
+    }
+    $response->getBody()->write("Invalid API key");
+    return $response;
 });
 $app->run();
 ?>

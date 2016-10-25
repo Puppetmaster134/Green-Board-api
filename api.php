877336036f657ede05aa25d2241c4de503d36ed1
Brian <bzimmerman@csumb.edu>
2016-10-25 04:34:44 -0400

    Changed filestructure

--- api.php
+++ api.php
@@ -3,6 +3,7 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 require '../vendor/autoload.php';
+require 'db_params.php';
 
 $config['displayErrorDetails'] = true;
 $config['addContentLengthHeader'] = false;
